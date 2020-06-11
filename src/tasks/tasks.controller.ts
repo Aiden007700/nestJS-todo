0ffdf3ce8d5f44d840a8958350c041711396f98e
aiden <campbell.e.aiden@gmail.com>
2020-06-11 19:15:27 +0200

    crud

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,22 +1,53 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Patch,
+  Query,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.model'
+import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDTO } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private tasksService: TasksService) { }
+  constructor(private tasksService: TasksService) {}
 
-    @Get()
-    getAllTasks(): Task[] {
-        return this.tasksService.getAllTasks();
+  @Get()
+  getTasks(@Query() filterDto: GetTasksFilterDTO): Task[] {
+    if (Object.keys(filterDto).length) {
+      this.tasksService.getTasksWithFilters(filterDto);
+    } else {
+      return this.tasksService.getAllTasks();
     }
+  }
 
-    @Post()
-    createTask(
-        @Body('title') title: string,
-        @Body('description') description: string,
-    ) {
-        return this.tasksService.createTask(title, description)
-    }
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Delete('/:id')
+  detereTaskById(@Param('id') id: string): Task {
+    return this.tasksService.deleteTaskById(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  // @Patch('/:id/status') <---- not generic but more strict!
+  @Patch('/:id/:what')
+  updateTaskById(
+    @Param('id') id: string,
+    @Param('what') what: string,
+    @Body('payload') payload: string,
+  ): Task {
+    return this.tasksService.updateTaskById(id, what, payload);
+  }
 }
-    
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,25 +1,68 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDTO } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
-    private tasks: Task[] = [];
+  private tasks: Task[] = [];
 
-    getAllTasks(): Task[] {
-        return this.tasks;
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  getTasksWithFilters(filterDto: GetTasksFilterDTO): Task[] {
+    const { status, search } = filterDto;
+    let tasks = this.getAllTasks();
+
+    if (status) {
+        tasks = tasks.filter(task => task.status === status)
     }
 
-    createTask(title: string, description: string): Task {
-        const task: Task = {
-            id: uuid(),
-            status: TaskStatus.OPEN,
-            title,
-            description
-        }
-
-        this.tasks.push(task);
-        return task;
+    if (search) {
+        tasks = tasks.filter(task => task.description.includes(search) || task.title.includes(search))
     }
+    return tasks;
+  }
 
+  getTaskById(id: string): Task {
+    return this.tasks.find(task => task.id === id);
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: uuid(),
+      status: TaskStatus.OPEN,
+      title,
+      description,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+
+  updateTaskById(id: string, what: string, payload: string): Task {
+    let updatedTask: Task | null;
+    this.tasks.forEach(task => {
+      if (task.id === id) {
+        task[what] = payload;
+        updatedTask = task;
+      }
+    });
+    return updatedTask;
+  }
+
+  deleteTaskById(id: string): Task {
+    let deletedTask: Task | null = null;
+    this.tasks = this.tasks.filter(task => {
+      if (task.id == id) {
+        deletedTask = task;
+      }
+      return task.id !== id;
+    });
+
+    return deletedTask;
+  }
 }
